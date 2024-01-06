486a2af227e809981d0f61353816e5f374320c65
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-06 18:16:38 +0700

    add initial go-fiber code

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	// "fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// create new fiber instance
+	app := fiber.New()
+
+	// home route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	// start server
+	app.Listen(":3000")
+}
