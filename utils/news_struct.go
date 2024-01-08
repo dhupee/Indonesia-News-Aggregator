e0d999a4c2f6bb44fed0eabfc30595ef262d4292
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-08 22:19:25 +0700

    update struct of Kompas

--- utils/news_struct.go
+++ utils/news_struct.go
@@ -1,10 +1,13 @@
 package utils
 
-type News struct {
+type KompasNews struct {
+	Url     string
 	Title   string
 	Author  string
+	Editor  string
 	Date    string
 	Source  string
 	Image   string // image url
 	Content string
+	Tags    []string
 }
