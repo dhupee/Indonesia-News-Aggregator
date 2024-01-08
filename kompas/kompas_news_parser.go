8509f012f8315dbe0404237f0429dd55d8deb1c7
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-09 00:36:02 +0700

    revised context extract, add data extract, add main function also

--- kompas/kompas_news_parser.go
+++ kompas/kompas_news_parser.go
@@ -4,31 +4,50 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/dhupee/Indonesia-News-Aggregator/utils"
+
 	"golang.org/x/net/html"
 )
 
-//TODO: add functions to append it to an object to be cleaned up
-func ExtractContent(rawHTML string, div string) string {
-	// Create a new tokenizer from the raw HTML
+KompasNews := utils.KompasNews{}
+
+// * This functions isnt used yet
+func newsContentCleanUp(rawNewsContent string) string {
+	cleanedNewsContent := []string{}
+
+	pattern := `Copyright 2008 - *`
+	regex := regexp.MustCompile(pattern)
+
+	for _, line := range strings.Split(rawNewsContent, ".") {
+		if !regex.MatchString(line) {
+			cleanedNewsContent = append(cleanedNewsContent, line)
+		}
+	}
+
+	return strings.Join(cleanedNewsContent, "\n")
+}
+
+func KompasExtractContentFromDiv(rawHTML string, div string) [] string {
 	tokenizer := html.NewTokenizer(strings.NewReader(rawHTML))
 
-	// Variable to track whether the current token is within a <p> tag
+	newsContent := []string{}
+
 	var inPTag bool
 
-	// Iterate through the tokens
+	// stopPattern := regexp.MustCompile("Copyright 2008 - 2023 PT. Kompas Cyber Media (Kompas Gramedia Digital Group).")
+
 	for {
 		tokenType := tokenizer.Next()
 
 		switch tokenType {
 		case html.ErrorToken:
-			// End of the document
-			fmt.Println("End of document")
-			return ""
+			log.Println("End of document")
+			return newsContent
+
 
 		case html.StartTagToken:
 			token := tokenizer.Token()
 
-			// Check if the current tag is a <p> tag
 			if token.Data == "p" {
 				inPTag = true
 			}
@@ -36,19 +55,44 @@ func ExtractContent(rawHTML string, div string) string {
 		case html.EndTagToken:
 			token := tokenizer.Token()
 
-			// Check if the current tag is the closing tag of a <p> tag
 			if inPTag && token.Data == "p" {
 				inPTag = false
 			}
 
 		case html.TextToken:
-			// If you want to extract text content within <p> tags, add your logic here
 			if inPTag {
 				text := strings.TrimSpace(tokenizer.Token().Data)
 				if text != "" {
-					fmt.Println("Text within <p> tag:", text)
+					log.Println("Extracting text:", text)
+					newsContent = append(newsContent, text)
 				}
 			}
 		}
 	}
+}
+
+func kompasExtractContentFromScriptTag(codeBlock string, pattern string) (string, error) {
+	// Compile the regular expression
+	re := regexp.MustCompile(pattern)
+
+	// Find the matches
+	matches := re.FindStringSubmatch(codeBlock)
+
+	// Check if a match was found
+	if len(matches) > 1 {
+		contentTitle := matches[1]
+		return contentTitle, nil
+	}
+
+	return "", fmt.Errorf("no match found")
+}
+
+func KompasGetContent(url string) KompasNews {
+	rawHTML := GetHTML(url)
+
+	KompasNews.Title := kompasExtractContentFromScriptTag(rawHTML, `content_title":\s*"([^"]+)"`)
+	KompasNews.Content := KompasExtractContentFromDiv(rawHTML, "read__content")
+
+
+	return KompasNews
 }
