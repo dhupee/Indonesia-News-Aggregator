99475962963db9605d0002b72111c8f55e942b90
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-09 20:02:14 +0700

    add regex to filter url coming in

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"regexp"
 
-	kompas "github.com/dhupee/Indonesia-News-Aggregator/kompas"
 	handlers "github.com/dhupee/Indonesia-News-Aggregator/handlers"
+	kompas "github.com/dhupee/Indonesia-News-Aggregator/kompas"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -21,7 +22,6 @@ func main() {
 	// Create a new Fiber app
 	app := fiber.New()
 
-
 	// Get the port from the environment variables
 	port := os.Getenv("PORT")
 
@@ -29,8 +29,8 @@ func main() {
 	app.Get("/", handlers.RootHandler)
 
 	// Define the route handlers for Kompas endpoints
-	app.Get("/kompas/search/:keyword?", KompasSearchHandler)
-	app.Get("/kompas/categories/:category?/:subcategories?", KompasCategoriesHandler)
+	app.Get("/kompas/search/", KompasSearchHandler)
+	app.Get("/kompas/categories/", KompasCategoriesHandler)
 	app.Get("/kompas/news/", KompasNewsHandler)
 
 	// Start the app on the specified port
@@ -38,7 +38,7 @@ func main() {
 }
 
 func KompasSearchHandler(c *fiber.Ctx) error {
-	keyword := c.Params("keyword")
+	keyword := c.Get("keyword")
 	if keyword == "" {
 		return c.SendString("Please specify keyword")
 	}
@@ -48,11 +48,13 @@ func KompasSearchHandler(c *fiber.Ctx) error {
 }
 
 func KompasCategoriesHandler(c *fiber.Ctx) error {
-	category := c.Params("category")
-	subcategories := c.Params("subcategories")
+	category := c.Get("category")
+	subcategories := c.Get("subcategories")
 
 	if subcategories == "" {
 		return c.SendString("You search for " + category)
+	} else {
+		return c.SendString("You search for " + subcategories + " in category " + category)
 	}
 
 	return c.SendString("You search for " + subcategories + " in category " + category)
@@ -61,11 +63,19 @@ func KompasCategoriesHandler(c *fiber.Ctx) error {
 func KompasNewsHandler(c *fiber.Ctx) error {
 	url := c.Get("Source")
 
-	if url == "" {
-		return c.SendFile("./kompas/error_text/kompas_news_handler.txt")
+	subDomainRegex := regexp.MustCompile(`^https?://(.+\.)*kompas\.com`)
+	if !subDomainRegex.MatchString(url) {
+		if url == "" {
+			return c.SendFile("./kompas/error_text/kompas_news_handler.txt")
+		}
+		// Reject the URL
+		domainRegex := regexp.MustCompile(`^https?://([^/]+)`)
+		matches := domainRegex.FindStringSubmatch(url)
+		if len(matches) > 1 {
+			return c.SendFile("rejected " + matches[1])
+		}
 	}
 
 	kompasNews := kompas.KompasGetData(url, &kompas.KompasNewsStruct{})
-
 	return c.JSON(kompasNews)
 }
