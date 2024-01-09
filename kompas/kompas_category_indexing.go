f735106b2619a8426df186a2c6c6c49a84a83146
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-10 00:40:46 +0700

    rename files, indexing sounds cooler

---
+++ kompas/kompas_category_indexing.go
@@ -0,0 +1,42 @@
+package kompas
+
+// import (
+// 	// "fmt"
+// 	// "log"
+// 	// "net/http"
+
+// 	"github.com/dhupee/Indonesia-News-Aggregator/utils"
+// )
+
+// var bola = []string{
+// 	"liga-italia",
+// 	"liga-inggris",
+// }
+
+// func KompasGetNewsList(rawHtml string) []string{
+// 	newsList := []string{}
+
+// 	return newsList
+// }
+
+// func KompasCategoryCheck(category string, subcategory string) bool {
+// 	var results bool
+
+// 	if category == "bola" {
+// 			// if subcategory in slice bola
+// 		if subcategory
+// 		}
+// 	} else {
+// 		results = false
+// 	}
+
+// 	return results
+// }
+
+
+// // TODO: implement this
+// // func KompasCategoriesListing(category string, subcategory string) []string{
+// // 	url := "http://" + category+ ".kompas.com/" + subcategory+ "/"
+
+
+// // }
