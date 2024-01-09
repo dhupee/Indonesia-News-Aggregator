badb6b7afaac4eb5954845789e57d1a82dbf7c7a
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-09 10:15:31 +0700

    organize handler file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	kompas "github.com/dhupee/Indonesia-News-Aggregator/kompas"
+	handlers "github.com/dhupee/Indonesia-News-Aggregator/handlers"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -20,23 +21,20 @@ func main() {
 	// Create a new Fiber app
 	app := fiber.New()
 
+
 	// Get the port from the environment variables
 	port := os.Getenv("PORT")
 
-	// Define the route handler for the root path
-	app.Get("/", rootHandler)
+	// Define the route handler for the root path and non-source-specific routes
+	app.Get("/", handlers.RootHandler)
 
 	// Define the route handlers for Kompas endpoints
 	app.Get("/kompas/search/:keyword?", kompasSearchHandler)
 	app.Get("/kompas/categories/:category?/:subcategories?", kompasCategoriesHandler)
-	app.Get("/kompas/news/:url?", kompasNewsHandler)
+	app.Get("/kompas/news/", kompasNewsHandler)
 
 	// Start the app on the specified port
 	app.Listen(":" + port)
-}
-
-func rootHandler(c *fiber.Ctx) error {
-	return c.SendFile("./assets/welcome.txt")
 }
 
 func kompasSearchHandler(c *fiber.Ctx) error {
@@ -61,12 +59,14 @@ func kompasCategoriesHandler(c *fiber.Ctx) error {
 }
 
 func kompasNewsHandler(c *fiber.Ctx) error {
-	url := c.Params("url")
+	url := c.Get("Source")
 
 	if url == "" {
 		errorText := "Please specify url\n\nExample: /kompas/news/https://otomotif.kompas.com/read/2024/01/06/180829115/pindad-bikin-prototipe-motor-listrik-ev-scooter-daya-jelajah-100-km"
 		return c.SendString(errorText)
 	}
 
-	return c.SendString("You search for " + url)
+	kompasNews := kompas.KompasGetContent(url, &kompas.KompasNewsStruct{})
+
+	return c.SendString(kompasNews.Title)
 }
