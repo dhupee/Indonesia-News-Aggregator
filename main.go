a8657b2dcb61b325cf8052091e4780449085ab60
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-01-12 10:19:30 +0700

    turns out I need to flip the switch case upside down

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 
 	kompas "github.com/dhupee/Indonesia-News-Aggregator/kompas"
 
@@ -52,37 +53,44 @@ func KompasSearchHandler(c *fiber.Ctx) error {
 }
 
 func KompasIndexHandler(c *fiber.Ctx) error {
-	category := c.Get("category")
-	page := c.Get("page")
-	date := c.Get("date")
+	// Header
+	page := c.Get("Page")
+	date := c.Get("Date")
+	category := c.Get("Category")
 
 	var url string
 
-	categoryValid := kompas.KompasCategoryCheck(category, kompas.KompasCategoryList)
-	if !categoryValid {
-		return c.SendString("Invalid category")
+	if category != "" {
+		categoryValid := kompas.KompasCategoryCheck(category, kompas.KompasCategoryList)
+		if !categoryValid {
+			return c.SendString("Invalid category, please specify one of the following: \n \n" + strings.Join(kompas.KompasCategoryList, ", "))
+		}
 	}
 
-	// // wait 3 seconds
-	// time.Sleep(3 * time.Second)
+	//make sure the date is YYYY-MM-DD
+	if date != "" {
+		if !regexp.MustCompile(`^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$`).MatchString(date) {
+			return c.SendString("Invalid date, please use YYYY-MM-DD format in your 'Date' header")
+		}
+	}
 
 	switch {
-	case category != "":
-		url = "https://indeks.kompas.com/?site=" + category
-	case page != "":
-		url = "https://indeks.kompas.com/?page=" + page
-	case date != "":
-		url = "https://indeks.kompas.com/?date=" + date
-	case category != "" && page != "":
-		url = "https://indeks.kompas.com/?site=" + category + "&page=" + page
-	case category != "" && date != "":
-		url = "https://indeks.kompas.com/?site=" + category + "&date=" + date
-	case page != "" && date != "":
-		url = "https://indeks.kompas.com/?page=" + page + "&date=" + date
 	case category != "" && page != "" && date != "":
 		url = "https://indeks.kompas.com/?site=" + category + "&page=" + page + "&date=" + date
+	case page != "" && date != "":
+		url = "https://indeks.kompas.com/?page=" + page + "&date=" + date
+	case category != "" && date != "":
+		url = "https://indeks.kompas.com/?site=" + category + "&date=" + date
+	case category != "" && page != "":
+		url = "https://indeks.kompas.com/?site=" + category + "&page=" + page
+	case date != "":
+		url = "https://indeks.kompas.com/?date=" + date
+	case page != "":
+		url = "https://indeks.kompas.com/?page=" + page
+	case category != "":
+		url = "https://indeks.kompas.com/?site=" + category
 	default:
-		url = "https://indeks.kompas.com/?site=tekno"
+		url = "https://indeks.kompas.com/"
 	}
 
 	// get news index
